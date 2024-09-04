8185a52801a9fcd2d939c238704575b1d5c0e242
Apicha660612162 <apichalerdjintanakid@gmail.com>
2024-09-04 16:08:32 +0700

    gg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            year="2024"
+            fullName="Apicha Lerdjintanakit"
+            studentId="660612162"
           />
         </div>
       </body>
